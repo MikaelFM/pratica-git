dd0dc26f2e500d51d2e6eac76e0bdc3413706dc2
Mikael Fernandes Moreira <mikaelfernandesmoreira@gmail.com>
2023-03-17 15:53:34 -0300

    Rotulando linhas e inserindo legenda no gráfico

--- geragrafico.py
+++ geragrafico.py
@@ -7,8 +7,11 @@ def main():
     leitor = LeitorArquivo('data.txt')
     valores = leitor.getValores()
     print(valores)
+    i = 1
     for serie in valores:
        plt.plot(serie)
+       i += 1
+    plt.legend(loc='upper left')
     plt.ylabel('Valores de entrada')
     plt.xlabel('Amostragem')
     plt.title('Gráfico de linhas')
